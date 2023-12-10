767897fcce3d5cd0ca4e92e8775d3674a73caa6f
SophiaAthena <147653452+SophiaAthena@users.noreply.github.com>
2023-12-10 10:15:50 -0500

    after error.js button app crashed, corrupt files. Uninstalled and installed json files and node modules, no fix.

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const ErrorPage = ({ message }) => {
+  const goBack = () => {
+    window.history.back();
+  };
+
   return (
     <div>
       <h1>Error</h1>
       <p>{message}</p>
+      <button onClick={goBack}>Return to Previous Page</button>
     </div>
   );
 };
